68e03a80238a227bae7e2f006325e43ed9f66143
Tafloh-crypto <cassanovah100@gmail.com>
2021-06-06 21:56:37 +0200

    initial commit
    Copied all the files from the local repository

---
+++ src/Banner.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import requests from "./request";
+import "./Banner.css";
+import movieTrailer from "movie-trailer";
+import YouTube from "react-youtube";
+
+const Banner = () => {
+  const [movie, setMovie] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchTrending);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  //console.log(movie);
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie.original_title || movie?.title || movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+          console.log(urlParams);
+        })
+        .catch((error) => console.log(movie));
+    }
+  };
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playsInline: true,
+    endSeconds: 3,
+    allowFullScreen: true,
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner_buttons">
+          <button onClick={() => handleClick(movie)} className="banner__button">
+            Play
+          </button>
+          <button className="banner__button">My List</button>
+        </div>
+
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
+      </div>
+      <div className="banner--fadeBottom"></div>
+      <div className="banner__Player">
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
+    </header>
+  );
+};
+
+export default Banner;
---
+++ src/Row.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import "./Row.css";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  //a snippet of code which runs based on a specific condition/variable
+  useEffect(() => {
+    //if [], run once when the row loads and dont run again
+    //if you pass in a property, it is goin to run once and every time the property changes ==> dependency
+    //await means when you send a request wait for the answer to come back
+
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      //console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie.original_title || movie?.title || movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+          console.log(urlParams);
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  return (
+    <div className="row">
+      <h1>{title}</h1>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {/* https://www.youtube.com/watch?v=4TqmUMUJYZY */}
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
+}
+
+export default Row;
